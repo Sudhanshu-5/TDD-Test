c52c46cbed27db2fd3215e5e00128e3c52ecff7d
Sudhanshu-5 <yawalkarsry@gmail.com>
2024-11-29 15:29:14 +0530

    feat: add test for single and multiple commma seperated values

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 export function Add(str) {
   //if str == ""
+
+  let sum = 0;
+  let numArr = [];
+
   if (str == "") {
     return 0;
+  } else {
+    numArr = str.split(",");
   }
+
+  sum = numArr.reduce((acc, curr) => {
+    const currNum = +curr;
+
+    return acc + currNum;
+  }, 0);
+
+  return sum;
 }
--- test/string-calculator.js
+++ test/string-calculator.js
@@ -5,4 +5,12 @@ describe("String calculator", () => {
   it("should return 0 if empty string is passed", () => {
     expect(Add("")).to.equal(0);
   });
+
+  it("should return digit if single char digit passed", () => {
+    expect(Add("1")).to.equal(1);
+  });
+
+  it("should return sum if multiple chars are passed", () => {
+    expect(Add("1,2,3")).to.equal(6);
+  });
 });
